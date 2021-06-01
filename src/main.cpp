cccfbc8b5c44f56cbdc7ede96cf69671b62bdf95
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2021-06-01 22:00:20 +0200

    Moved main.h to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
-#include "main.h"
+#include "mbed.h"
+#include "lcd.h"
+#include "touch.h"
+#include "gui.h"
+#include "io.h"
+#include "dome.h"
+
+//I2C
+extern I2C i2c;
+extern char i2cmembuff[9];      //buffer di scambio dati per i2c
+#define I2cMemAddr 0xA0         //indirizzo eerom su bus I2C
+#define i2cNoEnd    true
+#define i2cEnd      false
+
+//elenco inizio locazioni di memoria EEROM
+#define EeromTouchLoc   0   //16 locazioni per calibrazione touchscreen
+#define EeromParkLoc    16  //2 locazioni per quota parcheggio
 
 Ticker tick;
  
