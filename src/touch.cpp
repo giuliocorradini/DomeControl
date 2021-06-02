87ab46894d530aeba470d8926b7a569e71cbf79d
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2021-06-02 15:32:17 +0200

    Updated wait with ThisThread::sleep_for sleep_for requires a std::chrono argument

--- src/touch.cpp
+++ src/touch.cpp
@@ -195,7 +195,7 @@ void TouchSetngsSave(void){
     i2cmembuff[8] = temp;
     i2c.write(I2cMemAddr, i2cmembuff, 9, false);
 
-    ThisThread::sleep_for(0.01);                     //la eerom impiega almeno 5msec a scriversi
+    ThisThread::sleep_for(10ms);                     //la eerom impiega almeno 5msec a scriversi
     
     i2cmembuff[0] = 8;              //la eerom vuole come primo byte la locazione cui scrivere
     temp = Settings.TouchXoffset;
