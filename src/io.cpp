38503235a7f35d06c756eb9a2260b643a38372f0
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2022-01-18 16:27:17 +0100

    Fixed compilation error for MQTT merge

--- src/io.cpp
+++ src/io.cpp
@@ -6,7 +6,6 @@
 #include "gui.h"
 #include "WIZnetInterface.h"
 #include "config.h"
-#include "mqtt.h"
 
 BufferedSerial Pc(USBTX,USBRX);
 
@@ -141,10 +140,6 @@ void IoMain(void){
     
     //ogni secondo
     if (CycleCounter == 100){
-        //trasmetti la quota attuale verso il broker
-        char dome_pos_str[8];
-        sprintf(dome_pos_str, "%d", DomePosition);
-        MQTTController::publish("T1/cupola/pos", dome_pos_str, true);
         CycleCounter = 0;
     };
 
